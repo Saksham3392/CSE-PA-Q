008c0df9956a085eceb238e95dbdf992b9e7d603
SAKSHAM SHEORAN <saksham3392.beai24@chitkara.edu.in>
2026-09-18 00:46:13 +0530

    Delete CSE PA Q directory

--- CSE PA Q/app.py
+++
@@ -1,7 +0,0 @@
-# Alias entrypoint for Render and local servers
-from server import app
-
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 2090))
-    app.run(host="0.0.0.0", port=port, debug=True)
